7c58c2727082c2ccb8de8a7be65ca218d74fc56e
Vasnets0v <byvasnetsov@gmail.com>
2021-12-13 02:33:13 +0200

    add design with bootstrap

--- static/js/select_topic.js
+++ static/js/select_topic.js
@@ -1,4 +1,4 @@
 function create_new_topic(){
 var el = document.getElementById('new_topic')
-el.outerHTML = '<input name="topic" type="text">';
+el.outerHTML = '<input name="topic" type="text" class="form-control">';
 }
