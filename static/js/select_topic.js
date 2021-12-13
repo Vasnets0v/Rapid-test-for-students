fb3f0d3d0796a328dbc85582c3920ee0edf4b688
Vasnets0v <byvasnetsov@gmail.com>
2021-12-13 14:09:39 +0200

    completed test creation service

--- static/js/select_topic.js
+++ static/js/select_topic.js
@@ -1,4 +1,4 @@
 function create_new_topic(){
 var el = document.getElementById('new_topic')
-el.outerHTML = '<input name="topic" type="text" class="form-control">';
+el.outerHTML = '<input style="margin-bottom: 10px;" name="topic" type="text" class="form-control">';
 }
